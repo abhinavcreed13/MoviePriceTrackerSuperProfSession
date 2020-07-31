9811ad02d3f87181f23429ad831431f3962600c5
Abhinav Sharma <abhinav.sharma13@live.com>
2020-07-31 12:12:45 +1000

    Add project files.

---
+++ MoviePriceTrackerRestAPI/Controllers/TestApiController.cs
@@ -0,0 +1,31 @@
+﻿using MoviePriceTrackerRestAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MoviePriceTrackerRestAPI.Controllers
+{
+    public class TestApiController : ApiController
+    {
+        public string Get()
+        {
+            return "My TestApi is running!!";
+        }
+
+        public List<CustomListItem> GetMyItems()
+        {
+            List<CustomListItem> items = new List<CustomListItem>
+            {
+                new CustomListItem { Id = 1, Text = "text1" },
+                new CustomListItem { Id = 2, Text = "text2" },
+                new CustomListItem { Id = 3, Text = "text3" },
+                new CustomListItem { Id = 4, Text = "text4" },
+                new CustomListItem { Id = 5, Text = "text5" }
+            };
+            return items;
+        }
+    }
+}
---
+++ MoviePriceTrackerRestAPI/Startup.cs
@@ -0,0 +1,35 @@
+﻿using Owin;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace MoviePriceTrackerRestAPI
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // register my own middlewares
+
+            var config = new HttpConfiguration();
+
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                    name: "DefaultApi",
+                    routeTemplate: "api/{controller}/{action}/{id}",
+                    defaults: new { action = "Get", id = RouteParameter.Optional }
+                );
+
+            //XML thing
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+
+            app.UseWebApi(config);
+
+
+        }
+    }
+}
